15c6c723066ebd8317d2a828bb70379aab8bc468
Grigoriy <grigoriynbr@gmail.com>
2023-10-26 11:06:41 +0300

    Added meta

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+	 async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "upgrade-insecure-requests",
+          }
+        ],
+      },
+    ]
+  },
+}
